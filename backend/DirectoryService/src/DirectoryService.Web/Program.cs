49a235564d00fd456fb9ff5048bdf6810340d5fd
vladimLi <vladili.li@yandex.ru>
2026-06-18 16:14:16 +0500

    DS-1 completed

---
+++ backend/DirectoryService/src/DirectoryService.Web/Program.cs
@@ -0,0 +1,23 @@
+using Scalar.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOpenApi();
+
+builder.Services.AddControllers();
+
+builder.Services.AddHealthChecks();
+
+var app = builder.Build();
+
+app.MapControllers();
+
+app.MapHealthChecks("/health");
+
+if (!app.Environment.IsProduction())
+{
+    app.MapOpenApi();              // /openapi/v1.json
+    app.MapScalarApiReference(); // /scalar/v1
+}
+
+await app.RunAsync();
